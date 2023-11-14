b0b8b44d7c33f247af8657250787be3c75d04ff4
Tatyana Molchanova <mtguspeh@ukr.net>
2023-11-14 11:31:10 +0200

    error Metamask Welcome Back modal is showing on load not clicking on the connect wagmi button

---
+++ Untitled-1.ts
@@ -0,0 +1,199 @@
+<script setup lang="ts">
+import { Trade } from "../vuex/api/Trade";
+import { Header as HeaderManager } from "../vuex/api/Header";
+import { computed, onUnmounted } from "@vue/runtime-core";
+import { store } from "/@/vuex/store";
+import { onMounted, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { ACTIONS } from "/@/vuex/modules/enums/StoreEnums";
+import {
+    createWeb3Modal,
+    defaultWagmiConfig,
+    useWeb3Modal,
+    useWeb3ModalState,
+    useWeb3ModalTheme,
+} from "@web3modal/wagmi/vue";
+import { mainnet, arbitrum, polygon } from "@wagmi/core/chains";
+import eventBus from "/@/api/EventBus";
+
+
+const { t } = useI18n();
+let TradeManager;
+
+let usdcSizeSaved: string = "";
+let optionSizeSaved: string = "";
+
+
+const selectedFormState = computed(() => store.getters.selectedFormState).value;
+const web3Connected = computed(() => store.getters.getWeb3Connected).value;
+let userAcc: string | null = sessionStorage.getItem("account");
+const quoteRequest = computed(() => store.getters.quoteRequest).value;
+const legsCurrent = computed(() => store.getters.legsCurrent).value;
+const tradeForm = computed(() => store.getters.getTradeForm).value;
+const chainId = computed(() => store.getters.getChainId).value;
+let connectTogglerLabel = ref(t("connectWallet"));
+let isFirstLoad = false;
+let connected = ref(false);
+
+let modal;
+
+onMounted(() => {
+    isFirstLoad = false;
+    TradeManager = new Trade();
+
+    const projectId = import.meta.env.VITE_VUE_PROJECT_ID;
+
+    const metadata = {
+        name: 'Web3Modal',
+        description: 'Web3Modal Connect',
+        cacheProvider: false,
+        url: 'https://web3modal.com',
+        icons: ['https://avatars.githubusercontent.com/u/37784886']
+    }
+
+    const chains = [mainnet, arbitrum, polygon]
+
+    const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+    createWeb3Modal({ wagmiConfig, projectId, chains });
+    const { setThemeMode, setThemeVariables, themeMode, themeVariables } = useWeb3ModalTheme();
+
+    setThemeMode('dark');
+
+    setThemeVariables({
+        '--w3m-color-mix': '#6030f8',
+        '--w3m-color-mix-strength': 40
+    });
+
+    modal = useWeb3Modal();
+
+    eventBus.on('userAccount', (userAccount: string | null) => {
+        userAcc = userAccount;
+        connectToggler();
+        console.log('userAccount', userAccount);
+    });
+
+    userAcc = sessionStorage.getItem("account");
+
+    if (userAcc) {
+        const manager = new HeaderManager();
+        if (manager.minifyAddress(userAcc).startsWith("unde")) {
+            connectTogglerLabel.value = t("connectWallet");
+            console.log('UNDEFINED userAcc', userAcc);
+            return t("connectWallet");
+        }
+        console.log('userAcc', userAcc);
+        connectTogglerLabel.value = t("account") + manager.minifyAddress(userAcc);
+        return t("account") + manager.minifyAddress(userAcc);
+    }
+});
+
+onUnmounted(() => {
+    isFirstLoad = false;
+    console.log('onUnmounted', );
+    // sessionStorage.removeItem("account");
+});
+
+const connectToggler = async () => {
+    const manager = new HeaderManager();
+    const accounts = await (window as any).ethereum.request({method: 'eth_accounts'});
+    const account = accounts[0];
+    sessionStorage.setItem("account", account);
+
+    store.dispatch(ACTIONS.USER_ACCOUNT, account);
+    if (!account && !userAcc) {
+        connectTogglerLabel.value = t("connectWallet");
+        return t("connectWallet");
+    } else {
+        connectTogglerLabel.value = t("account") + manager.minifyAddress(account);
+        return t("account") + manager.minifyAddress(account);
+    }
+};
+
+let usdcSize = tradeForm.usdcSize;
+
+let optionSize = tradeForm.optionSize;
+
+const connect = async () => {
+    connected = ref(true);
+    isFirstLoad = true;
+    eventBus.emit("connectButtonConnect", userAcc);
+    usdcSizeSaved = tradeForm.usdcSize;
+    optionSizeSaved = tradeForm.optionSize;
+    const selectedNetwork = computed(() => store.getters.selectedNetwork).value || localStorage.getItem("selectedNetwork");
+    const chainID = computed(() => store.getters.getChainId).value;
+    await store.dispatch(ACTIONS.CONNECT_USER, chainID);
+
+    const projectId = import.meta.env.VITE_VUE_PROJECT_ID;
+
+    const metadata = {
+        name: 'Web3Modal',
+        description: 'Web3Modal Connect',
+        url: 'https://web3modal.com',
+        icons: ['https://avatars.githubusercontent.com/u/37784886'],
+    }
+
+    const chains = [mainnet, arbitrum, polygon];
+    const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+    createWeb3Modal({ wagmiConfig, projectId, chains });
+
+
+    const { setThemeMode, setThemeVariables, themeMode, themeVariables } = useWeb3ModalTheme();
+
+    setThemeMode('dark');
+
+    modal = useWeb3Modal();
+
+    store.dispatch(ACTIONS.CHANGE_CHAIN, selectedNetwork);
+    store.dispatch(ACTIONS.CHANGE_NETWORK, selectedNetwork);
+    console.log('connect CONNECT BUTTON CHANGE CHAIN selectedNetwork', selectedNetwork);
+
+    await connectToggler();
+    const { open, selectedNetworkId } = useWeb3ModalState();
+
+    if (!open) {
+        await connectToggler();
+    }
+
+    optionSize = optionSizeSaved;
+    usdcSize = usdcSizeSaved;
+    // await TradeManager.updateUserBalances();
+
+    userAcc = sessionStorage.getItem("account");
+    console.log('userAcc', userAcc);
+
+    if (userAcc) {
+        const manager = new HeaderManager();
+        if (manager.minifyAddress(userAcc).startsWith("unde")) {
+            connectTogglerLabel.value = t("connectWallet");
+            return t("connectWallet");
+        }
+        connectTogglerLabel.value = t("account") + manager.minifyAddress(userAcc);
+        return t("account") + manager.minifyAddress(userAcc);
+    }
+
+    // optionSize = optionSizeSaved;
+    // usdcSize = usdcSizeSaved;
+    // // await TradeManager.updateUserBalances();
+    //
+    // store.dispatch(ACTIONS.CHANGE_CHAIN, selectedNetwork);
+    // store.dispatch(ACTIONS.CHANGE_NETWORK, selectedNetwork);
+    // console.log('connect CONNECT BUTTON CHANGE CHAIN selectedNetwork', selectedNetwork);
+    connected = ref(false);
+    window.location.reload();
+};
+</script>
+
+<template>
+    <w3m-connect-button
+        :label="connectTogglerLabel"
+        @click="connect"
+    />
+</template>
+
+
+<style scoped>
+    w3m-connect-button {
+       margin-right: 10px;
+    }
+</style>
